7ad854a236719352c0f7e5cebeba3ecd79e9d59a
renjithvk <me@renjithvk.com>
2017-07-19 23:44:40 +0530

    Default options

--- src/js/pre.js
+++ src/js/pre.js
@@ -1,6 +1,11 @@
 
 (function() {
 	this.Pre = function(options = {}) {
-		//		
+
+		// Default options
+		this.el = (options.el) ? options.el : '.pre';
+		this.spaces = (options.spaces == undefined) ? true : (options.spaces);
+		this.newline = (options.newline == undefined) ? true : (options.newline);
+		
 	}
 }());
