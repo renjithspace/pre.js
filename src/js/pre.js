7438c5826acc89710fdc62077496580dc006ae8b
renjithvk <me@renjithvk.com>
2017-07-19 23:45:20 +0530

    Get element

--- src/js/pre.js
+++ src/js/pre.js
@@ -6,6 +6,9 @@
 		this.el = (options.el) ? options.el : '.pre';
 		this.spaces = (options.spaces == undefined) ? true : (options.spaces);
 		this.newline = (options.newline == undefined) ? true : (options.newline);
+
+		// Get element
+		let element = document.querySelector(this.el);
 		
 	}
 }());
