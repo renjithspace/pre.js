c8763e58159ceb7209eddcccede0fbaec8f31fb8
renjithvk <me@renjithvk.com>
2017-10-07 22:44:03 +0530

    First build

---
+++ dist/js/pre.js
@@ -0,0 +1,116 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+
+(function() {
+	this.Pre = function() {
+
+		// Default options
+		this.options = {
+			el: '.pre',
+			spaces: false,
+			newlines: true,
+		};
+
+		// Replace default options with arguments
+		if (arguments.length && typeof arguments[0] === 'object') {
+			optionsReplacer(this.options, arguments[0]);
+		}
+
+		// Default options replacer
+		function optionsReplacer(options, args) {
+			for (let arg in args) {
+				if ( options.hasOwnProperty(arg) ) {
+					options[arg] = args[arg];
+				}
+			}
+		}
+
+		// Get element
+		let element = document.querySelector(this.options.el);
+
+		// Preserve
+		if(element) {
+			if(this.options.newlines) {
+				let preNewlines = element.innerHTML.replace(/\n/g, '</br>');
+				element.innerHTML = preNewlines;
+			}
+
+			if(this.options.spaces) {
+				let preSpaces = element.innerHTML.replace(/\s/g, '&nbsp');
+				element.innerHTML = preSpaces;
+			}
+			return;
+		}
+	}
+}());
+
+
+/***/ })
+/******/ ]);
