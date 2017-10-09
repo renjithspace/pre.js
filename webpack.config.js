13a2253bf800fcee0727213187f3bc82be5dccdc
renjithvk <me@renjithvk.com>
2017-10-09 22:40:45 +0530

    UglifyES webpack plugin configure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyESPlugin = require('uglify-es-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/pre.js',
+    entry: {
+        app: './src/js/pre.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist/js/']),
+        new UglifyESPlugin()
+    ],
     output: {
         filename: 'pre.js',
         path: path.resolve(__dirname, 'dist/js/')
